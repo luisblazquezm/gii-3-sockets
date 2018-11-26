18444aaefb49650b8ebe896ada71302e2c44c3f7
Samuel Gómez Sánchez <samuelg@usal.es>
2018-11-26 18:02:42 +0100

    26/11/19 18:02

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "./log/log.h"
+
+int main(int argc, char *argv[])
+{
+    init_log_file("log.txt");
+    
+    return 0;
+}
---
+++ src/utils/utils.h
@@ -0,0 +1,9 @@
+#ifndef __UTILS_H
+#define __UTILS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char* strfmt(const char* format, ...);
+
+#endif
